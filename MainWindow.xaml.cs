cb104cf4add7919be7f5a17d7c536552dd49a1bc
David_Calvo <david.calvo@mataro.epiaedu.cat>
2024-10-04 18:40:01 +0200

    teclado

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,37 +26,73 @@ namespace Calculadora
         double num1 = 0;
         double num2 = 0;
         string tipoOperacion = "";
+        bool punto = false;
+        bool acabado = false;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button numeroBoton = sender as Button;
             string contenidoBoton = numeroBoton.Content.ToString();
+
+            if (acabado == true)
+            {
+                MainText.Text = "0";
+                acabado = false;
+            }
+
 
 
             // Manejar el texto primncipal
             if (MainText.Text == "0")
             {
                 MainText.Text = contenidoBoton;
-                
+                if (contenidoBoton == ",")
+                {
+                    MainText.Text = "0,";
+                    punto = true;
+                }
+
+
             }
             else
             {
                 if (MainText.Text.Length < 18)
                 {
+                    if(contenidoBoton == ",")
+                    {
 
-                    
-                    MainText.Text += contenidoBoton;
+                        if (punto == false)
+                        {
+                            MainText.Text += ",";
+                            punto = true;
+                        }
+
+
+                    }
+                    else
+                    {
+                        
+                        MainText.Text += contenidoBoton;
+                    }
+
                 }
             }
         }
 
+
+
+
+
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            
+
             if (e.Key >= Key.D0 && e.Key <= Key.D9)
             {
                 int numero = e.Key - Key.D0;
@@ -76,8 +112,80 @@ namespace Calculadora
                 else
                 {
                     MainText.Text = "0";
+                    punto = false;
                 }
             }
+            else if (e.Key == Key.Escape)
+            {
+
+                MainText.Text = "0";
+                SecondaryText.Text = "";
+                num1 = 0;
+                num2 = 0;
+                resultado = 0;
+                tocaOperar = false;
+                punto = false;
+                acabado = false;
+            }else if(e.Key == Key.Enter)
+            {
+                   
+                Igual_Click(equal,null);
+
+            }
+            else if (e.Key == Key.Add)
+            {
+
+                operacion(sumar, null);
+
+            }
+            else if (e.Key == Key.Subtract)
+            {
+                operacion(restar, null);
+            }
+            else if (e.Key == Key.Multiply)
+            {
+                operacion(multiply, null);
+            }
+            else if (e.Key == Key.Divide)
+            {
+                operacion(dividir, null);
+            }
+            else if (e.Key == Key.Decimal)
+            {
+                Button_Click(comma, null);
+            }
+            else if (e.Key == Key.OemComma)
+            {
+                Button_Click(comma, null);
+            }
+            else if (e.Key == Key.OemPeriod)
+            {
+                Button_Click(comma, null);
+            }
+            else if (e.Key == Key.OemPlus)
+            {
+                operacion(sumar, null);
+            }
+            else if (e.Key == Key.OemMinus)
+            {
+                operacion(restar, null);
+            }
+           
+            else if (e.Key == Key.OemPeriod)
+            {
+                Button_Click(comma, null);
+            }
+            
+            else if (e.Key == Key.OemPlus)
+            {
+                operacion(sumar, null);
+            }
+            else if (e.Key == Key.OemMinus)
+            {
+                operacion(restar, null);
+            }
+           
+       
         }
 
         private Button GetButtonByNumber(int numero)
@@ -112,7 +220,10 @@ namespace Calculadora
         private void operacion(object sender, RoutedEventArgs e)
         {
 
+        
+
             tipoOperacion = (sender as Button).Content.ToString();
+            
             if(tocaOperar == false)
             {
                 num1 = double.Parse(MainText.Text);
@@ -123,7 +234,7 @@ namespace Calculadora
                 num2 = double.Parse(MainText.Text);
                 CalcularResultado();
             }
-            SecondaryText.Text += MainText.Text + " " + tipoOperacion;
+            SecondaryText.Text += MainText.Text + " " + tipoOperacion + " ";
 
             MainText.Text = "0";
 
@@ -167,11 +278,15 @@ namespace Calculadora
             MainText.Text = resultado.ToString();
             SecondaryText.Text = "";
             tocaOperar = false;
+            punto = false;
+            acabado = true;
 
 
         }
     }
 }
+
+
 
 
 
